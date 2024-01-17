e9deaff9702f7d9306d9641772d75ec77db846a9
Shay Eclipse K. Stage <ShayKStage@protonmail.com>
2024-01-17 16:06:06 +0100

    remove rust-tools.nvim

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.snippets")
-require("plugin_config.rust")
-require("plugin_config.nvim-dap")
 require("plugin_config.liveserver")
